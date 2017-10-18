50d02abaa6ad282c8cecebeda7af6042c75caa49
zifupeng <util@botframework.com>
2017-10-18 01:53:48 +0000

    prepare to setup source control

---
+++ app.js
@@ -0,0 +1,22 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: '57c819f2-f915-4ae2-a8c2-69fcb5502930',
+    appPassword: 'Yr3rWu8iuWceKACFGob2tpi',
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
