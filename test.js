f96cad00631cead7fe91e893b0681faa497d0112
Weiqiao Li <vickysg0210@gmail.com>
2017-11-13 12:39:18 +0800

    microsoft access connection

--- test.js
+++
@@ -1,5 +0,0 @@
-var chartGenerator = require('./chartGenerator');
-console.log("hehe?????");
-// chartGenerator.try();
-// console.log(chartGenerator.functions.quantityByCategory());
-// chartGenerator.functions['quantityByCategory']();
